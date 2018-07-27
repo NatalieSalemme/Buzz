86f95bf6d99e8f7ac692f7fb43e2cd800756fcbd
Natalie Salemme <nataliesalemme@gmail.com>
2018-07-27 11:53:42 -0700

    font size is 25px and li are red

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -8,12 +8,10 @@ let fizzText = document.querySelector('.fizzText');
 let buzzText = document.querySelector('.buzzText');
 let fizzBuzzText = document.querySelector('.fizzBuzzText');
 let everythingText = document.querySelector('.everythingText');
-//
 
 
+fizz.addEventListener('click', function listing() {
 
-
-function listing(){
   let d = document;
   let main = d.getElementsByTagName('main')[ 0 ];
   let ul = d.createElement( 'ul' );
@@ -23,12 +21,13 @@ function listing(){
     var li = d.createElement( 'li' );
     if(i % 3 ===0) {
       li.textContent = " fizz";
+      li.style.fontSize = "25px";
+      li.style.color = "red";
       ul.appendChild( li );
-  } else{
-    li.textContent = i;
-    ul.appendChild(li);               
+    } else{
+      li.textContent = i;
+      ul.appendChild(li);
 
+    }
   }
-  }
-}
-listing();
+});
