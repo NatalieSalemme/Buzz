e922b8364c07067ce6686ff5c66885bf84b343fd
Natalie Salemme <nataliesalemme@gmail.com>
2018-07-27 11:40:27 -0700

    Fizzbuzz using li elements

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -4,83 +4,31 @@ let buzz = document.querySelector('#buzz');
 let fizzbuzz = document.querySelector('#fizzbuzz');
 let everything = document.querySelector('#everything');
 let allButtons = document.querySelector('.allButtons');
+let fizzText = document.querySelector('.fizzText');
 let buzzText = document.querySelector('.buzzText');
 let fizzBuzzText = document.querySelector('.fizzBuzzText');
 let everythingText = document.querySelector('.everythingText');
 //
-fizz.addEventListener('click', function() {
-  let emptyFizzArr = [];
-  let fizzArr = [];
-  for(let j = 0; j <=100; j++) {
-    emptyFizzArr.push(j);
+
+
+
+
+function listing(){
+  let d = document;
+  let main = d.getElementsByTagName('main')[ 0 ];
+  let ul = d.createElement( 'ul' );
+  let i;
+  main.appendChild( ul );
+  for( i = 1; i < 100; i = i + 1 ){
+    var li = d.createElement( 'li' );
+    if(i % 3 ===0) {
+      li.textContent = " fizz";
+      ul.appendChild( li );
+  } else{
+    li.textContent = i;
+    ul.appendChild(li);               
+
   }
-  for (let i = 1; i < emptyFizzArr.length; i++) {
-  if(i % 3 === 0) {
-    fizzArr.push(i + " fizz" + "<br>");
-  }  else {
-    fizzArr.push(i + "<br>");
-  }
-  }
-endText.innerHTML = fizzArr.join('');
-console.log(fizzArr);
-});
-
-
-
-  buzz.addEventListener('click', function() {
-    let emptyBuzzArr = [];
-    let buzzArr = [];
-    for(let k = 0; k <=100; k++) {
-      emptyBuzzArr.push(k);
-      console.log(k);
-    }
-  for (let m = 1; m < emptyBuzzArr.length; m++) {
-    if(m % 5 === 0) {
-      buzzArr.push(m + " buzz" + "<br>");
-    } else {
-      buzzArr.push(m + "<br>");
-    }
-  }
-  console.log(buzzText.innerHTML = buzzArr.join(''));
-});
-
-
-
-fizzbuzz.addEventListener('click', function() {
-  let emptyFizzBuzzArr = [];
-  let fizzBuzzArr = [];
-  for(let n = 0; n <=100; n++) {
-    emptyFizzBuzzArr.push(n);
-    console.log(n);
-  }
-for (let o = 1; o < emptyFizzBuzzArr.length; o++) {
-  if(o % 3 === 0 && o % 5 === 0) {
-    fizzBuzzArr.push(o + " fizzbuzz" + "<br>");
-  } else {
-    fizzBuzzArr.push(o + "<br>");
   }
 }
-fizzBuzzText.innerHTML = fizzBuzzArr.join('');
-});
-
-
-  everything.addEventListener('click', function() {
-    let emptyEverythingArr = [];
-    let everythingArr = [];
-    for(let p = 0; p <=100; p++) {
-      emptyEverythingArr.push(p);
-      console.log(emptyEverythingArr);
-    }
-  for (let q = 1; q < emptyEverythingArr.length; q++) {
-    if(q % 3 === 0 && q % 5 === 0) {
-      everythingArr.push(q + " fizzbuzz" + "<br>");
-    } else if(q % 3 === 0) {
-      everythingArr.push(q + " fizz" + "<br>");
-    } else if(q % 5 === 0) {
-      everythingArr.push(q + " buzz" + "<br>");
-    } else {
-      everythingArr.push(q + "<br>");
-    }
-  }
-    console.log(everythingText.innerHTML = everythingArr.join(''));
-});
+listing();
